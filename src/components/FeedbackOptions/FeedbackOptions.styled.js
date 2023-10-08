d6e9fc72156e73abb14bf56d998741aa3bf7de9a
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-10-08 22:27:59 +0200

    + hover

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -5,11 +5,13 @@ export const Button = styled.button`
   border-radius: ${p => p.theme.radius.sm};
   padding: ${p => p.theme.spacing(3)};
   backgraund-color: ${p => p.theme.colors.btn_color}
-  :hover & {color: ${p => p.theme.colors.blue}}
+   &:hover {color: ${p => p.theme.colors.blue}}
+    &:focus {color: ${p => p.theme.colors.blue}}
 `;
 
 export const ButtonList = styled.div`
     display: flex;
     flex-direction: row;
     gap: ${p => p.theme.spacing(5)} ;
+
 `
